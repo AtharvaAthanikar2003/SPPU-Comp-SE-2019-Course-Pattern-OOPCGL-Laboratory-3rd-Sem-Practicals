138a930fc069e17ef6585e0bc6ab4e4d42ae4098
Atharva Athanikar <120152105+AtharvaAthanikar2003@users.noreply.github.com>
2022-12-22 19:44:24 +0530

    Update OOP Exp.1-Grp.A-Exp.1.cpp

--- OOP Exp.1-Grp.A-Exp.1.cpp
+++ OOP Exp.1-Grp.A-Exp.1.cpp
@@ -1,15 +1,14 @@
 /*
-Implement a class Complex which represents the Complex Number data type. Implement
-the following
+Implement a class Complex which represents the Complex Number data type. Implement the following :-
 1. Constructor (including a default constructor which creates the complex number 0+0i).
 2. Overloaded operator+ to add two complex numbers.
-3. Overloaded operator* to multiply two complex numbers.4. Overloaded << and >> to
-print and read Complex Numbers.
+3. Overloaded operator* to multiply two complex numbers.
+4. Overloaded << and >> to print and read Complex Numbers.
 */
 
 #include <iostream>
 using namespace std;
-class Complex // decaring Class Complex
+class Complex // declaring Class Complex
 {
     double real;
     double img;
